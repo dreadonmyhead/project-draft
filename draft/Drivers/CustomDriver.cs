7351bc00b4c5b8d6b3bc6cde5b150bdc2e0b0ac5
Vera <vdjakova@gmail.com>
2020-11-22 19:01:46 +0200

    Add project files.

---
+++ draft/Drivers/CustomDriver.cs
@@ -0,0 +1,37 @@
+﻿namespace draft.Drivers
+
+{
+    public class CustomDriver
+    {
+        public static IWebDriver GetChromeDriver()
+        {
+            return GetDriver(Browsers.Chrome);
+        }
+
+        public static IWebDriver GetFirefoxDriver()
+        {
+            return GetDriver(Browsers.Firefox);
+        }
+
+        private static IWebDriver GetDriver(Browsers browserName)
+        {
+            IWebDriver driver = null;
+
+            switch (browserName)
+            {
+                case Browsers.Firefox:
+                    driver = new FirefoxDriver();
+                    break;
+                case Browsers.Chrome:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            return driver;
+        }
+    }
+}
+
---
+++ draft/Tools/MyScreenshot.cs
@@ -0,0 +1,18 @@
+﻿namespace draft.Tools
+{
+    public class MyScreenshot
+    {
+        public static void MakeScreeshot(IWebDriver driver)
+        {
+            Screenshot myBrowserScreenshot = driver.TakeScreenshot();
+            string screenshotDirectory = Path.GetDirectoryName(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
+            string screenshotFolder = Path.Combine(screenshotDirectory, "screenshots");
+            Directory.CreateDirectory(screenshotFolder);
+            string screenshotName = $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:HH-mm}.png";
+            string screenshotPath = Path.Combine(screenshotFolder, screenshotName);
+            myBrowserScreenshot.SaveAsFile(screenshotPath, ScreenshotImageFormat.Png);
+        }
+    }
+}
